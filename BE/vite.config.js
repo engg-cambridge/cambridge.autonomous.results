2ff7c3ad656840739582e21f150cb8c1e3c7dbad
engg - cambridge <ganeshgabbar582@gmail.com>
2025-04-28 19:54:34 +0530

    Update vite.config.js

--- BE/vite.config.js
+++ BE/vite.config.js
@@ -3,21 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
   build: {
-    outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom'],
-        },
-      },
-    },
-  },
-  server: {
-    port: 3000
+    outDir: 'dist'
   }
 })
+
+
 
 
